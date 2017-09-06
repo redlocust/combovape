c942a1576b4014e647b20ef2935403243d507ebd
Кузнецов <redlocust1@gmail.com>
2017-09-06 17:29:41 +0300

    elementary server

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '../public')));
+
 app.listen(3000, () => {
   console.log('start server on port 3000');
 });
-
-console.log('start server');
