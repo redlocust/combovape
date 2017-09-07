f200c39b8d6d5125bb6d4cda3fec5be051ae854d
Кузнецов <redlocust1@gmail.com>
2017-09-07 18:46:55 +0300

    some fix

--- server/server.js
+++ server/server.js
@@ -5,6 +5,19 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../public')));
 
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res) {
+  res.send('im the home page!');
+});
+
+// about page route (http://localhost:8080/about)
+app.get('/about', function(req, res) {
+  res.send('im the about page!');
+});
+
+
 app.listen(3000, () => {
   console.log('start server on port 3000');
 });
