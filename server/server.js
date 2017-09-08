e48edb19822673818873649a928327a6230e0690
Кузнецов <redlocust1@gmail.com>
2017-09-08 15:04:28 +0300

    some fix

--- server/server.js
+++ server/server.js
@@ -3,14 +3,14 @@ import path from 'path';
 
 const app = express();
 
-//app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', function(req, res) {
-  res.send('im the home page!');
-});
+// app.get('/', function(req, res) {
+//   res.send('im the home page!');
+// });
 
 // about page route (http://localhost:8080/about)
 app.get('/about', function(req, res) {
