a34379495d9243cf5ece221634be5a12cb1c2cef
alexei Kuznecov <redlocust1@gmail.com>
2017-09-18 00:36:10 +0300

    mock up

--- server/controllers/mix.controller.js
+++ server/controllers/mix.controller.js
@@ -2,66 +2,63 @@ import Mix from '../models/mix';
 import cuid from 'cuid';
 
 export function getMixes(req, res) {
-  Mix.find({}, function (err, mixes) {
-    console.log(mixes);
-    if (err) {
-      res.status(500).send();
-    }
-    res.json({mixes});
-  })
+    Mix.find({}, function (err, mixes) {
+        console.log(mixes);
+        if (err) {
+            res.status(500).send();
+        }
+        res.json({mixes});
+    })
 }
 
 export function getMix(req, res) {
-  res.send(
-    {
-      message: req.params.mixcode
-    }
-  );
+    res.send(
+        {
+            message: req.params.mixcode
+        }
+    );
 }
 
 
 export function deleteMix(req, res) {
-  res.send(
-    {
-      message: `Delete : ${req.params.mixcode}`
-    }
-  );
+    res.send(
+        {
+            message: `Delete : ${req.params.mixcode}`
+        }
+    );
 }
 
 export function deleteMixes(req, res) {
-  res.send(
-    {
-      message: 'Delete all mixes'
-    }
-  );
+    res.send(
+        {
+            message: 'Delete all mixes'
+        }
+    );
 }
 
 export function addMix(req, res) {
 
-  console.log("req.body! ", req.body);
+    for (let i = 1; i <= 10; i++) {
 
-  // if (!req.body.code) {
-  //   res.status(403).end();
-  // }
+        let newMix = new Mix({
+            title: `${i} mix`,
+            author: `${i} author`,
+            cuid: cuid()
+        });
 
-  const newMix = new Mix({
-    title: 'Firstic',
-    author: 'Redlocust',
-    cuid: cuid()
-  });
-
-  newMix.save((err, saved) => {
-    if (err) {
-      res.json(err);
+        newMix.save((err, saved) => {
+            if (err) {
+                res.json(err);
+            }
+            res.json({post: saved});
+        });
     }
-    res.json({post: saved});
-  });
 }
 
 export function updateMix(req, res) {
-  res.send(
-    {
-      message: `Update: ${req.params.mixcode}`
-    }
-  );
+    res.send(
+        {
+            message: `Update: ${req.params.mixcode}`
+        }
+    );
 }
