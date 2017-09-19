bbbcdce3163ff6e976e35295401acdac56476be9
alexei Kuznecov <redlocust1@gmail.com>
2017-09-20 01:26:48 +0300

    fix addMix function with sample data

--- server/controllers/mix.controller.js
+++ server/controllers/mix.controller.js
@@ -2,65 +2,66 @@ import Mix from '../models/mix';
 import cuid from 'cuid';
 
 export function getMixes(req, res) {
-  Mix.find({}, function (err, mixes) {
-    console.log(mixes);
-    if (err) {
-      res.status(500).send();
-    }
-    res.json(mixes);
-  })
+    Mix.find({}, function (err, mixes) {
+        console.log(mixes);
+        if (err) {
+            res.status(500).send();
+        }
+        res.json(mixes);
+    })
 }
 
 export function getMix(req, res) {
-  res.send(
-    {
-      message: req.params.mixcode
-    }
-  );
+    res.send(
+        {
+            message: req.params.mixcode
+        }
+    );
 }
 
 
 export function deleteMix(req, res) {
-  res.send(
-    {
-      message: `Delete : ${req.params.mixcode}`
-    }
-  );
+    res.send(
+        {
+            message: `Delete : ${req.params.mixcode}`
+        }
+    );
 }
 
 export function deleteMixes(req, res) {
-  res.send(
-    {
-      message: 'Delete all mixes'
-    }
-  );
+    res.send(
+        {
+            message: 'Delete all mixes'
+        }
+    );
 }
 
 export function addMix(req, res) {
 
-  for (let i = 1; i <= 10; i++) {
+    let mixArray = [];
 
-    let newMix = new Mix({
-      title: `${i} mix`,
-      author: `${i} author`,
-      cuid: cuid()
-    });
+    for (let i = 1; i <= 10; i++) {
 
-    newMix.save((err, saved) => {
-      if (err) {
-        res.json(err);
-      } else {
-        res.json({post: saved})
-      }
-      ;
-    });
-  }
+        let newMix = new Mix({
+            title: `${i} mix`,
+            author: `${i} author`,
+            cuid: cuid()
+        });
+
+        mixArray.push(newMix);
+    }
+
+    Mix.insertMany(mixArray)
+        .then((mixes) => {
+            res.json({post: mixes})
+        })
+        .catch((err) => res.json(err))
 }
 
 export function updateMix(req, res) {
-  res.send(
-    {
-      message: `Update: ${req.params.mixcode}`
-    }
-  );
+    res.send(
+        {
+            message: `Update: ${req.params.mixcode}`
+        }
+    );
 }
